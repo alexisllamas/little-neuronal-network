dd723bed4ed836ee29ba3996b8f08c177dcaa468
Alexis Navarro <alexisnavarro23@gmail.com>
2018-10-30 02:16:50 -0600

    prettier!

--- src/episodes/Three.js
+++ src/episodes/Three.js
@@ -1,81 +1,79 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
-import {generatePoints, rand, generateOnePoint, sleep} from '../utils'
+import { generatePoints, rand, generateOnePoint, sleep } from '../utils';
 import Graph from '../UI/Graph';
 import Button from '../UI/Button';
 
-
 const numberOfPoints = 250;
 
-const team = point => point.x > point.y ? 1 : -1
+const team = point => (point.x > point.y ? 1 : -1);
 
 const guess = (weights, point) => {
-  const sum =
-        point.x * weights.x +
-        point.y * weights.y
+  const sum = point.x * weights.x + point.y * weights.y;
 
-  const team = sum >= 0 ? 1 : -1
+  const team = sum >= 0 ? 1 : -1;
 
-  return team
-}
+  return team;
+};
 
 const generateRandomWeights = () => ({
   x: rand(-1, 1),
-  y: rand(-1, 1)
-})
+  y: rand(-1, 1),
+});
 
 function train(weights, point, team) {
-  const guessResult = guess(weights, point) // 1
-  const error = team - guessResult
+  const guessResult = guess(weights, point); // 1
+  const error = team - guessResult;
   return {
     x: weights.x + point.x * error,
     y: weights.y + point.y * error,
-  }
+  };
 }
 
 function Four() {
-  const [randomPoints, setPoints] = useState(generatePoints(numberOfPoints))
-  const regeneratePoints = () => setPoints(generatePoints(numberOfPoints))
+  const [randomPoints, setPoints] = useState(generatePoints(numberOfPoints));
+  const regeneratePoints = () => setPoints(generatePoints(numberOfPoints));
 
   const [timesTrained, setTimesTrained] = useState(0);
 
-  const [weights, setWeights] = useState(generateRandomWeights())
+  const [weights, setWeights] = useState(generateRandomWeights());
   const trainNetwork = () => {
-    const point = generateOnePoint()
-    const correctResult = team(point)
+    const point = generateOnePoint();
+    const correctResult = team(point);
 
     setWeights(weights => {
-      const newWeights = train(weights, point, correctResult)
-      setTimesTrained(times => times + 1)
-      return newWeights
-    })
-  }
+      const newWeights = train(weights, point, correctResult);
+      setTimesTrained(times => times + 1);
+      return newWeights;
+    });
+  };
 
   const trainNetworkTimes = async times => {
-    const iterator = Array.from({length: times})
+    const iterator = Array.from({ length: times });
     for (const _ of iterator) {
       // await sleep(25)
-      trainNetwork()
+      trainNetwork();
     }
-  }
+  };
 
   const trainInfinite = async () => {
-    while(true) {
-      await sleep(100)
-      trainNetworkTimes(100)
+    while (true) {
+      await sleep(100);
+      trainNetworkTimes(100);
     }
-  }
+  };
 
   return (
     <>
       <Graph points={randomPoints} getTeam={point => guess(weights, point)} />
-      <div className="times-trained">
-        Times trained: {timesTrained}
-      </div>
+      <div className="times-trained">Times trained: {timesTrained}</div>
       <div className="weights">
         x = {weights.x} y = {weights.y}
       </div>
-      <div className="button-container" style={{display: 'flex', justifyContent: 'space-around'}}>
+      <div
+        className="button-container"
+        style={{ display: 'flex', justifyContent: 'space-around' }}
+      >
         <Button onClick={regeneratePoints}>Regenerate points</Button>
         <Button onClick={trainNetwork}>Train the system!</Button>
         <Button onClick={() => trainNetworkTimes(100)}>x100!</Button>
@@ -83,7 +81,7 @@ function Four() {
         <Button onClick={() => trainInfinite()}>xinfinite!</Button>
       </div>
     </>
-  )
+  );
 }
 
-export default Four
+export default Four;
--- src/episodes/Two.js
+++ src/episodes/Two.js
@@ -1,43 +1,46 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
-import {generatePoints, rand} from '../utils'
+import { generatePoints, rand } from '../utils';
 import Graph from '../UI/Graph';
 import Button from '../UI/Button';
-
 
 const numberOfPoints = 250;
 
 const guess = (weights, point) => {
-  const sum =
-        point.x * weights.x +
-        point.y * weights.y
+  const sum = point.x * weights.x + point.y * weights.y;
 
-  const team = sum >= 0 ? 1 : -1
+  const team = sum >= 0 ? 1 : -1;
 
-  return team
-}
+  return team;
+};
 
 const generateRandomWeights = () => ({
   x: rand(-1, 1),
-  y: rand(-1, 1)
-})
+  y: rand(-1, 1),
+});
 
 function Two() {
-  const [randomPoints, setPoints] = useState(generatePoints(numberOfPoints))
-  const regeneratePoints = () => setPoints(generatePoints(numberOfPoints))
+  const [randomPoints, setPoints] = useState(generatePoints(numberOfPoints));
+  const regeneratePoints = () => setPoints(generatePoints(numberOfPoints));
 
-  const [randomWeights, setRandomWeights] = useState(generateRandomWeights())
-  const regenerateWeights = () => setRandomWeights(generateRandomWeights())
+  const [randomWeights, setRandomWeights] = useState(generateRandomWeights());
+  const regenerateWeights = () => setRandomWeights(generateRandomWeights());
 
   return (
     <>
-      <Graph points={randomPoints} getTeam={point => guess(randomWeights, point)} />
-      <div className="button-container" style={{display: 'flex', justifyContent: 'space-around'}}>
+      <Graph
+        points={randomPoints}
+        getTeam={point => guess(randomWeights, point)}
+      />
+      <div
+        className="button-container"
+        style={{ display: 'flex', justifyContent: 'space-around' }}
+      >
         <Button onClick={regeneratePoints}>Regenerate points</Button>
         <Button onClick={regenerateWeights}>Regenerate weights</Button>
       </div>
     </>
-  )
+  );
 }
 
-export default Two
+export default Two;
